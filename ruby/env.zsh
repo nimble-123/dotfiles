9279a230ffbd25f5ce371d02a0d5052a71e7bfc1
Nils Lutz <info@nilslutz.de>
2018-01-28 17:37:00 +0100

    🔨 Add colorls tab completion

--- ruby/env.zsh
+++ ruby/env.zsh
@@ -2,3 +2,5 @@ export RBENV_ROOT="$HOME/.homebrew/opt/rbenv"
 export GEM_HOME="$HOME/.homebrew/opt/gems"
 export GEM_PATH="$GEM_HOME"
 export PATH="$HOME/.rbenv/bin:$GEM_HOME/bin:$PATH"
+
+source $(dirname $(gem which colorls))/tab_complete.sh
