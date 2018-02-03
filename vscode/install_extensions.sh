213a9ec7a016b2a13aa535ceed30def666b38312
Nils Lutz <info@nilslutz.de>
2018-02-03 19:12:36 +0100

    📦 Add Docker plugin for VScode

--- vscode/install_extensions.sh
+++ vscode/install_extensions.sh
@@ -2,5 +2,6 @@ code --install-extension \
   EditorConfig.EditorConfig \
   Equinusocio.vsc-material-theme \
   PKief.material-icon-theme \
+  PeterJausovec.vscode-docker \
   esbenp.prettier-vscode \
   formulahendry.terminal
