f1f3c036a09ebc523db443460c3f216b89a03c14
Nils Lutz <info@nilslutz.de>
2018-03-17 18:03:19 +0100

    ➕ Add JS specific development plugins

--- vscode/install_extensions.sh
+++ vscode/install_extensions.sh
@@ -3,5 +3,11 @@ code --install-extension \
   Equinusocio.vsc-material-theme \
   PKief.material-icon-theme \
   PeterJausovec.vscode-docker \
+  Zignd.html-css-class-completion \
+  christian-kohler.path-intellisense \
+  dbaeumer.vscode-eslint \
+  eamodio.gitlens \
   esbenp.prettier-vscode \
-  formulahendry.terminal
+  formulahendry.terminal \
+  joelday.docthis \
+  mikestead.dotenv
