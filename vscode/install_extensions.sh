f4d81f5a32c2b63788dac54864f92582f543e42b
Nils Lutz <info@nilslutz.de>
2018-08-05 19:03:40 +0200

    🔧: Update VScode settings
    Added symlink for conventional commits with gitmoji style.

--- vscode/install_extensions.sh
+++ vscode/install_extensions.sh
@@ -4,24 +4,27 @@ code --install-extension \
   Hirse.vscode-ungit \
   James-Yu.latex-workshop \
   JuanBlanco.solidity \
+  KnisterPeter.vscode-commitizen \
+  MS-CEINTL.vscode-language-pack-de \
   PKief.material-icon-theme \
   PeterJausovec.vscode-docker \
   Zignd.html-css-class-completion \
   christian-kohler.path-intellisense \
   codezombiech.gitignore \
+  coolbear.systemd-unit-file \
   dbaeumer.vscode-eslint \
   eamodio.gitlens \
   esbenp.prettier-vscode \
+  evilz.vscode-reveal \
   formulahendry.auto-close-tag \
   formulahendry.terminal \
-  James-Yu.latex-workshop \
   jebbs.plantuml \
   joelday.docthis \
   michalkozubik.ui5-explorer \
   mikestead.dotenv \
-  MS-CEINTL.vscode-language-pack-de \
   octref.vetur \
   redhat.java \
+  tht13.html-preview-vscode \
   vscjava.vscode-java-debug \
   vscjava.vscode-java-pack \
   vscjava.vscode-java-test \
